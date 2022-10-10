3c9c8a626705f9a13108bcf90ea9810b77864f7a
Prashasti Garg <gargprashasti16@gmail.com>
2022-10-10 20:48:48 +1100

    Initial commit

---
+++ api.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import uvicorn
+from pyngrok import ngrok
+import nest_asyncio
+from nltk.stem import WordNetLemmatizer
+from nltk import data
+import joblib
+
+data.path.append('C:\\Users\\sarth\\Downloads\\news detection\\nltk_data\\')
+
+
+class Model:
+    def __init__(self, model={}, tfvec={}):
+        self.filename = 'TA40final.joblib'
+        self.model = model
+#         self.vectorization = TfidfVectorizer()
+
+    def load(self):
+        self.model = joblib.load(self.filename)
+
+    # This is the code can be called on the server, ie. in the api route. The model should be passed to it and data from the user can be retrieved from request body or query params.
+    def predict(self, data):
+        return round(self.model.predict_proba([data])[0][1]*10)
+
+    # Call this method to save the model on the disk.
+    def save(self):
+        joblib.dump(self.model, self.filename)
+
+
+def text_process(mess):
+    """
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Perform lemmatization
+    4. Returns a list of the cleaned text
+    """
+    lemma = WordNetLemmatizer()
+    nopunc = lemma.lemmatize(mess)
+    # Now just remove any stopwords
+    return nopunc
+
+
+model = Model()
+model.load()
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Data(BaseModel):
+    text: str
+
+
+@app.get("/")
+async def root_get():
+    return {"message": "Hello World"}
+
+
+@app.post("/")
+async def root_post(text: Data):
+    print(text.text)
+    prediction = model.predict(text.text)
+    return {"prediction": prediction}
+
+
+# ngrok_tunnel = ngrok.connect(8080)
+# print("Public URL:", ngrok_tunnel.public_url)
+# nest_asyncio.apply()
+uvicorn.run(app, port=4769)
